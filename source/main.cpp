56c82092fb639c7b581005cf2443ee75d74efcd6
djunior <brittodavid@yahoo.com>
2014-02-12 23:40:28 -0200

    Adicionando primeira versão do main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *	Projeto Tactics
+ *
+ *  Created on: Feb 12, 2014
+ *      Author: djunior
+ */
+#include <iostream>
+#include <stdio.h>
+
+int main(){
+	std::cout << "Iniciando jogo Tactics!" << std::endl;
+	return 0;
+}
+
