a988c05a22499c28cb0f84cc76d2ab4b16f908f9
djunior <brittodavid@yahoo.com>
2014-02-20 00:07:16 -0300

    Primeira versao do SDL funcional

--- source/main.cpp
+++ source/main.cpp
@@ -13,27 +13,53 @@
 #include "GameManager.h"
 #include "Unit.h"
 
+#include "SDL2/SDL.h"
+
 using namespace std;
 
-int main(){
-	cout << "Iniciando jogo Tactics!" << endl;
+int main(int argc, char *argv[]){
 
-	cout << "Criando tabuleiro!" << endl;
-	Board gameBoard(8,8);
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
 
-	cout << "Criando GameManager!" << endl;
-	GameManager gm(&gameBoard);
+	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480,
+		SDL_WINDOW_SHOWN);
+	if (win == 0){
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
 
-	cout << "Criando unidade 1!" << endl;
-	gm.createUnit("knight",TEAM_A);
+	std::cin.get();
 
-	cout << "Criando unidade 2!" << endl;
-	gm.createUnit("wizard",TEAM_A);
+//	std::cout << "Iniciando jogo Tactics!" << std::endl;
+//
+//	std::cout << "Criando tabuleiro!" << std::endl;
+//	Board gameBoard(8,8);
+//
+//	std::cout << "Criando GameManager!" << std::endl;
+//	GameManager gm(&gameBoard);
+//
+//	std::cout << "Criando unidade 1!" << std::endl;
+//	gm.createUnit("knight",TEAM_A);
+//
+//	std::cout << "Criando unidade 2!" << std::endl;
+//	gm.createUnit("wizard",TEAM_A);
+//
+//	gameBoard.debug_showMap();
+//
+//	gm.startGame();
 
-	gameBoard.debug_showMap();
 
-	gm.startGame();
 
+
+
+
+
+
+//	SDL_Quit();
 	return 0;
 }
 
