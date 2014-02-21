e2d57e4ad9673363fd02408b6782fc1e3bd9162f
djunior <brittodavid@yahoo.com>
2014-02-21 00:21:22 -0300

    ae porra

--- source/main.cpp
+++ source/main.cpp
@@ -14,6 +14,7 @@
 #include "Unit.h"
 
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
 
 using namespace std;
 
@@ -24,42 +25,83 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480,
-		SDL_WINDOW_SHOWN);
-	if (win == 0){
+    bool Running = true;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Event Event;
+
+    SDL_Texture *Track,*Car;
+    SDL_Rect rectTrack,rectCar;
+
+    // OnInit
+
+    window = SDL_CreateWindow("Image Loading", 100, 100, 1280, 720, SDL_WINDOW_RESIZABLE | SDL_RENDERER_PRESENTVSYNC);
+	if (window == 0){
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED );
+    if (renderer == NULL){
+        std::cout << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-	std::cin.get();
+    //Load Content
+    int w=0,h=0;
+    Track = IMG_LoadTexture(renderer,"mapa.jpg");
+    SDL_QueryTexture(Track, NULL, NULL, &w, &h);
+    rectTrack.x = 0;
+    rectTrack.y = 0;
+    rectTrack.w = w;
+    rectTrack.h = h;
 
-//	std::cout << "Iniciando jogo Tactics!" << std::endl;
-//
-//	std::cout << "Criando tabuleiro!" << std::endl;
-//	Board gameBoard(8,8);
-//
-//	std::cout << "Criando GameManager!" << std::endl;
-//	GameManager gm(&gameBoard);
-//
-//	std::cout << "Criando unidade 1!" << std::endl;
-//	gm.createUnit("knight",TEAM_A);
-//
-//	std::cout << "Criando unidade 2!" << std::endl;
-//	gm.createUnit("wizard",TEAM_A);
-//
-//	gameBoard.debug_showMap();
-//
-//	gm.startGame();
+    Car = IMG_LoadTexture(renderer, "char_lanca.png");
+    SDL_QueryTexture(Car, NULL, NULL, &w, &h);
+    rectCar.x = 350;
+    rectCar.y = 200;
+    rectCar.w = w/2;
+    rectCar.h = h;
 
+    SDL_Rect src;
+    src.x=0;
+    src.y=0;
+    src.w=w/4;
+    src.h=h/2;
 
+    while (Running) {
+        while(SDL_PollEvent(&Event)) {
+        	switch(Event.type)
+			{
+				case SDL_QUIT:
+					Running = false;
+					break;
 
+				case SDL_KEYDOWN:
+					switch(Event.key.keysym.sym)
+					{
+						case SDLK_ESCAPE:
+							Running = false;
+							break;
+						case SDLK_RIGHT:
+							rectCar.x+=10;
+							break;
+						case SDLK_LEFT:
+							rectCar.x-=10;
+							break;
+					}
+					break;
 
+			}
+        }
 
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, Track, NULL, &rectTrack);
+		SDL_RenderCopy(renderer, Car, &src, &rectCar);
+		SDL_RenderPresent(renderer);
+    }
 
-
-
-//	SDL_Quit();
+	SDL_Quit();
 	return 0;
 }
 
