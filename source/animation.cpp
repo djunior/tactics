f8b80b8c312161c1e466d631c13776c955ce5580
djunior <brittodavid@yahoo.com>
2014-03-18 16:06:14 -0300

    adicionando classe Animation

---
+++ source/animation.cpp
@@ -0,0 +1,53 @@
+/*
+ * animation.cpp
+ *
+ *  Created on: Mar 17, 2014
+ *      Author: djunior
+ */
+
+#include "animation.h"
+
+Animation::Animation(ANIMATION_TYPE t,BOARD_POINT start,BOARD_POINT end,int dur) {
+	type = t;
+
+	currentFrame = 0;
+	duration = dur;
+
+	startPoint = start;
+	endPoint = end;
+}
+
+// Default initiation;
+Animation::Animation(){
+	type = ANIMATION_IDLE;
+
+	currentFrame = 1;
+	duration = 1;
+
+	startPoint.x = 0;
+	startPoint.y = 0;
+
+	endPoint.x = 0;
+	endPoint.y = 0;
+}
+
+Animation::~Animation() {
+	// TODO Auto-generated destructor stub
+}
+
+bool Animation::hasStarted(){
+	return currentFrame > 0;
+}
+
+bool Animation::hasEnded(){
+	return currentFrame >= duration;
+}
+
+void Animation::debug_showStats(){
+	std::cout << "Animation(" << this << ") stats:" << std::endl;
+	std::cout << "Type: " << type << std::endl;
+	std::cout << "Duration: " << duration << std::endl;
+	std::cout << "Current Frame: " << currentFrame << std::endl;
+	std::cout << "Start Point(" << &startPoint << "): (" << startPoint.x << "," << startPoint.y << ")" << std::endl;
+	std::cout << "End Point(" << &endPoint << "): (" << endPoint.x << "," << endPoint.y << ")" << std::endl;
+}
---
+++ source/animation.h
@@ -0,0 +1,39 @@
+/*
+ * animation.h
+ *
+ *  Created on: Mar 17, 2014
+ *      Author: djunior
+ */
+
+#ifndef ANIMATION_H_
+#define ANIMATION_H_
+
+#include "utils/board_utils.h"
+#include <iostream>
+
+enum ANIMATION_TYPE{
+	ANIMATION_IDLE,
+	ANIMATION_UNIT_MOVE,
+	ANIMATION_UNIT_ATTACK
+};
+
+typedef enum ANIMATION_TYPE ANIMATION_TYPE;
+
+class Animation {
+public:
+	BOARD_POINT startPoint, endPoint;
+	ANIMATION_TYPE type;
+	double currentFrame,duration;
+
+	Animation(ANIMATION_TYPE,BOARD_POINT, BOARD_POINT,int);
+	Animation();
+
+	bool hasStarted();
+	bool hasEnded();
+
+	virtual ~Animation();
+
+	void debug_showStats();
+};
+
+#endif /* ANIMATION_H_ */
