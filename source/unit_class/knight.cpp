c9b62e76edd9b050e7b007814308076d1fe8b91b
djunior <brittodavid@yahoo.com>
2014-05-18 19:33:00 -0300

    Implementando troca de frame quando o Knight morre

--- source/unit_class/knight.cpp
+++ source/unit_class/knight.cpp
@@ -32,41 +32,33 @@ Knight::~Knight() {
 
 // Configura um SDL_Rect para apontar um frame da unidade parada.
 void Knight::selectFrameIdle(SDL_Rect *rect){
-    rect->x = 181;
-    rect->y = 3;
-    rect->w = 136;
-    rect->h = 123;
+	if (isDead() == true) {
+		rect->x = 0;
+		rect->y = 741;
+		rect->w = 136;
+		rect->h = 123;
+	} else {
+		rect->x = 181;
+		rect->y = 3;
+		rect->w = 136;
+		rect->h = 123;
+	}
 }
 
 // Configura um SDL_Rect para apontar um frame da animacao de movimento, baseado no valor de frameIndex
 void Knight::selectFrameMove(int frameIndex, SDL_Rect* rect){
-
 	rect->w = 136;
 	rect->h = 111;
-
 	rect->x = 0 + (44 + rect->w)*(frameIndex);
 	rect->y = 188;
-
 }
 
 // Configura um SDL_Rect para apontar um frame da animacao de ataque, baseado no valor de frameIndex
 void Knight::selectFrameAttack(int frameIndex, SDL_Rect* rect){
-
-//	if (frameIndex < 6){
-//		rect->w = 136;
-//		rect->h = 107;
-//
-//		rect->x = (44 + rect->w)*(frameIndex);
-//		rect->y = 372;
-//
-//	} else {
-		rect->w = 136;
-		rect->h = 122;
-
-		rect->x = 181 + (44 + rect->w)*(frameIndex);
-		rect->y = 733;
-//	}
-
+	rect->w = 136;
+	rect->h = 122;
+	rect->x = 181 + (44 + rect->w)*(frameIndex);
+	rect->y = 733;
 }
 
 void Knight::selectFrame(int frameIndex, ANIMATION_TYPE type, SDL_Rect* rect){
